28b6f80ca76e7f21e841c31bdbf45823de0ebb45
Maryam <mary.tkb.90@gmail.com>
2022-06-28 17:04:16 +0100

    first game

--- manual_rps.py
+++ manual_rps.py
@@ -3,12 +3,14 @@ import random
 
 def get_user_choice():
     user_choices = input('Welcome to the game!Please select rock, paper or scissors:')
+    
     return user_choices
 
 
 def get_computer_choice():
     choice= random.choice(['rock', 'paper', 'scissors'])
     print('Computer chose:' + choice)
+    
     return choice
     
     
